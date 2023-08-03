038497c3b2af161c87948badb4d7800c2289e383
mateuscavedini <mateuscvdn@gmail.com>
2023-08-03 19:50:00 -0300

    Initial commit

---
+++ src/test/java/io/github/mateuscavedini/moviesapi/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateuscavedini.moviesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
